a90c007c8ad6c42ad206f6bde685ab31286432db
Guilherme <xgui@mailbox.org>
2022-04-19 22:24:17 +0100

    clean up

--- contracts/Logger.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.4.22 <0.9.0;
-
-// Its a way for designer to say that
-// "any child of the abstract contract has to implement specified methods"
-
-abstract contract Logger {
-}
